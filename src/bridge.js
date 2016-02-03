740384d3e8faa13bc0da5cad5a5db660023b6168
Martin Trojer <martin.trojer@gmail.com>
2016-02-03 08:51:57 +0000

    cleanup

--- src/bridge.js
+++ src/bridge.js
@@ -11,42 +11,40 @@ var broker = 'mqtt://mqtt.opensensors.io',
 var log4js = require('log4js'),
     log = log4js.getLogger('osio-bridge');
 log.setLevel('INFO');
-log4js.configure({
-    appenders: [{type: 'console'}]});
+log4js.configure({appenders: [{type: 'console'}]});
 
 var mqtt  = require('mqtt'),
     job = null;
 
 var mqtt_client = mqtt.connect(broker,{
-    clientId: clientId,
-    username: username,
-    password:  password
+  clientId: clientId,
+  username: username,
+  password: password
 });
 
 mqtt_client.on('connect', function () {
-    log.info('Connected');
-    job = setInterval(publish_data, 10 * 1000);
-    publish_data('test');
+  log.info('Connected');
+  job = setInterval(publish_data, 10 * 1000);
 });
 
 mqtt_client.on('close', function () {
-    if (job != null) {
-        clearInterval(job);
-        job = null;
-    }
-    log.info('Connection closed');
+  if (job != null) {
+    clearInterval(job);
+    job = null;
+  }
+  log.info('Connection closed');
 });
 
 var exec = require('child_process').exec;
 
 function publish_data() {
-    exec("usr/local/bin/pcsensor", function(error,stdout,stderr) {
-	if (error != null) {
-	    log.error(error);
-	    log.error(stderr);
-	} else {
-	    log.info('Publishing... '+stdout);
-	    mqtt_client.publish(topic, stdout);
-	}	    
-    });
+  exec("usr/local/bin/pcsensor", function(error,stdout,stderr) {
+    if (error != null) {
+      log.error(error);
+      log.error(stderr);
+    } else {
+      log.info('Publishing... '+stdout);
+      mqtt_client.publish(topic, stdout);
+    }
+  });
 }
